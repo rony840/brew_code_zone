1a25777eaa5ad2c46af4eaaf2612b566a526ba66
Ronit Fozdar <ronitfozdar99@gmail.com>
2026-03-01 18:51:32 -0600

    created student management system sql file with basic queries

---
+++ learning_SQL_and_Python/weekly_challenges/week_1/student_management_system.sql
@@ -0,0 +1,40 @@
+/*
+====================================================
+|   Week 1
+|   Mini Project 1 — Student Management System
+|   Authors: brew_code_zone, Ronit Fozdar
+====================================================
+
+Requirements:
+- Create Database 'StudentDB'
+- Create 'Students' table (student_id, name, age, course)
+- Insert student records
+- Fetch student data using SELECT
+- Remove all records (but keep table schema)
+- Delete table completely (including schema)
+
+*/
+
+CREATE DATABASE StudentDB;
+
+CREATE TABLE Students(
+    student_id SERIAL PRIMARY KEY,
+    student_name VARCHAR(50),
+    age INT,
+    course_name VARCHAR(30),
+);
+
+INSERT INTO Students(student_name, age, course_name) VALUES
+('Alice', '24', 'Data Science');
+
+INSERT INTO Students(student_name, age, course_name) VALUES
+('John', '20', 'Python'),
+('Sara', '21', 'Deep Learning'),
+('Samantha', '19', 'Statistics-1'),
+('Robert', '22', 'PostgreSQL');
+
+SELECT * FROM Students;
+
+TRUNCATE TABLE Students;
+
+DROP TABLE Students;
